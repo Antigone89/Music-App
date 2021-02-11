410f149ef9b4be1a225997446f9fe7263d556320
Anja888 <42465595+Anja888@users.noreply.github.com>
2021-02-11 22:44:29 +0100

    Login

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import AlbumList from './Components/AlbumList'
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Searchbar from './Components/Searchbar';
 import Navigationbar from './Components/Navigationbar';
 import RegistrationScreen from './Components/RegistrationScreen';
 import AlbumDetails from './Components/AlbumDetails'
 import { MusicProvider } from './Components/MusicContext';
-import { Login } from '.Components/RegistrationScreen';
+import Login from './Components/Login';
 
 
 function App() {
+
+  const [inputText, setInputText] = useState("");
+
   return (
+   
     <BrowserRouter>
       <MusicProvider>
     <div className="App">
         <header className="App-header"> 
-          < Navigationbar />
-          <AlbumDetails />
+            < Navigationbar />
           <Switch>
             <Route exact path="/Searchbar" component={Searchbar}>
             </Route>
+            <Route exact path="/Login" component={Login}>
+            </Route>
+            <Route exact path="/RegistrationScreen" component={RegistrationScreen} setInputText={setInputText}>
+            </Route>  
             <Route exact path="/" component={AlbumList}>
             </Route>
-            <Route exact path="/RegistrationScreen" component={RegistrationScreen}>
-            </Route>
-      </Switch>
-  
-      </header>
+          </Switch>
+        </header>
         </div>
         </MusicProvider>
       </BrowserRouter>
